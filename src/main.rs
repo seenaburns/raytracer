04929cd785fe2a53bbf2b78bd77876e320bd5c24
Seena Burns <hello@seenaburns.com>
2017-07-16 21:50:34 -0700

    Add Ray data structure
    - Move vec3 into own file
    - Use snake_case for vec3
    - Add convenience method for creating vec3
    - Add Tests for Ray

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate raytracer;
 use raytracer::vec3::Vec3;
+use raytracer::ray::Ray;
 
 use std::io::{self, Write};
 
@@ -10,12 +11,12 @@ fn main() {
     io::stdout().write_fmt(format_args!("P3\n{} {}\n{}\n", NX, NY, 255)).unwrap();
     for j in (0..NY).rev() {
         for i in 0..NX {
-            let color = Vec3 {
-                x: (i as f64) / (NX as f64),
-                y: (j as f64) / (NY as f64),
-                z: 0.2f64
-            };
-            let color = color.mulScalar(255.99);
+            let color = Vec3::new(
+                (i as f64) / (NX as f64),
+                (j as f64) / (NY as f64),
+                0.2f64
+            );
+            let color = color.mul_scalar(255.99);
             println!("{} {} {}", color.x as i32, color.y as i32, color.z as i32);
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,31 @@
+use ::vec3::Vec3;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub dir: Vec3
+}
+
+impl Ray {
+    pub fn new(o: Vec3, d: Vec3) -> Ray {
+        Ray { origin: o, dir: d }
+    }
+
+    pub fn point_at_parameter(self, t: f64) -> Vec3 {
+        self.origin + self.dir.mul_scalar(t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_point_at_parameter() {
+        let r = Ray {
+            origin: Vec3::new(1.0,0.0,0.0),
+            dir: Vec3::new(1.0,2.0,3.0),
+        };
+        assert!(r.point_at_parameter(0.5) == Vec3::new(1.5,1.0,1.5))
+    }
+}
