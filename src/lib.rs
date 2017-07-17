d093782a42f51f376a933a54c63ab03328fc94c7
Seena Burns <hello@seenaburns.com>
2017-07-16 20:09:13 -0700

    Chapter 2, add vec3 struct in a crate

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+pub mod vec3 {
+    use std::ops::*;
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Vec3 {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+    }
+
+    impl Vec3 {
+        pub fn r(&self) -> &f64 {
+            &self.x
+        }
+
+        pub fn g(&self) -> &f64 {
+            &self.x
+        }
+
+        pub fn b(&self) -> &f64 {
+            &self.x
+        }
+
+        pub fn map(self, f: &Fn(f64) -> f64) -> Vec3 {
+            Vec3 { x: f(self.x), y: f(self.y), z: f(self.z), }
+        }
+
+        pub fn addScalar(self, other: f64) -> Vec3 {
+            self.map(&(|x: f64| x + other))
+        }
+
+        pub fn subScalar(self, other: f64) -> Vec3 {
+            self.map(&(|x: f64| x - other))
+        }
+
+        pub fn mulScalar(self, other: f64) -> Vec3 {
+            self.map(&(|x: f64| x * other))
+        }
+
+        pub fn divScalar(self, other: f64) -> Vec3 {
+            self.map(&(|x: f64| x / other))
+        }
+
+        pub fn length(self) -> f64 {
+            let len = self.squared_length();
+            len.sqrt()
+        }
+
+        pub fn squared_length(self) -> f64 {
+            self.x*self.x + self.y*self.y * self.z*self.z
+        }
+
+        pub fn normalized(self) -> Vec3 {
+            self.divScalar(self.length())
+        }
+
+        pub fn dot(self, other: Vec3) -> f64 {
+            self.x*other.x + self.y*other.y + self.z*other.z
+        }
+
+        pub fn cross(self, other: Vec3) -> Vec3 {
+            Vec3 {
+                x:  (self.y * other.z - self.z * other.y),
+                y: -(self.x * other.z - self.z * other.x),
+                z:  (self.x * other.y - self.y * other.x),
+            }
+        }
+    }
+
+    impl Add for Vec3 {
+        type Output = Vec3;
+        fn add(self, other: Vec3) -> Vec3 {
+            Vec3 { x: self.x + other.x, y: self.y + other.y, z: self.z + other.z, }
+        }
+    }
+
+    impl Sub for Vec3 {
+        type Output = Vec3;
+        fn sub(self, other: Vec3) -> Vec3 {
+            Vec3 { x: self.x - other.x, y: self.y - other.y, z: self.z - other.z, }
+        }
+    }
+
+    impl Mul for Vec3 {
+        type Output = Vec3;
+        fn mul(self, other: Vec3) -> Vec3 {
+            Vec3 { x: self.x * other.x, y: self.y * other.y, z: self.z * other.z, }
+        }
+    }
+
+    impl Div for Vec3 {
+        type Output = Vec3;
+        fn div(self, other: Vec3) -> Vec3 {
+            Vec3 { x: self.x - other.x, y: self.y - other.y, z: self.z - other.z, }
+        }
+    }
+
+    impl Neg for Vec3 {
+        type Output = Vec3;
+        fn neg(self) -> Vec3 {
+            Vec3 { x: -self.x, y: -self.y, z: -self.z, }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate raytracer;
+use raytracer::vec3::Vec3;
+
 use std::io::{self, Write};
 
 const NX: i32 = 200;
@@ -6,14 +9,14 @@ const NY: i32 = 100;
 fn main() {
     io::stdout().write_fmt(format_args!("P3\n{} {}\n{}\n", NX, NY, 255)).unwrap();
     for j in (0..NY).rev() {
-        for i in (0..NX) {
-            let r = (i as f32) / (NX as f32);
-            let g = (j as f32) / (NY as f32);
-            let b = 0.2f32;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
-            println!("{} {} {}", ir, ig, ib);
+        for i in 0..NX {
+            let color = Vec3 {
+                x: (i as f64) / (NX as f64),
+                y: (j as f64) / (NY as f64),
+                z: 0.2f64
+            };
+            let color = color.mulScalar(255.99);
+            println!("{} {} {}", color.x as i32, color.y as i32, color.z as i32);
         }
     }
 }
